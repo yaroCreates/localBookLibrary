7a201dcd268faee54bf3724b343ee9cf792b81ab
James Yaro <onuhjamesyaro@gmail.com>
2021-07-15 14:31:21 +0100

    Designed the UI and created the script

---
+++ app.js
@@ -0,0 +1,18 @@
+//Book class: Representing the book
+class Book {
+    constructor(name, author, status) {
+        this.name = name
+        this.author = author
+        this.status = status
+    }
+}
+
+//Design class: Handling the page tasks
+
+//Store class: for holding the books
+
+//Event: Add a book
+
+//Event: Display book
+
+//Event: Remove book
